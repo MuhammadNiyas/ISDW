9cfa58acf37b276dbc7af0b840554b0efd4a249c
asira25 <129973344+asira25@users.noreply.github.com>
2023-06-21 23:51:19 +0800

    Add files via upload

--- cart1.php
+++ cart1.php
@@ -7,6 +7,13 @@ if (!isset($_SESSION['buyerID'])) {
     exit();
 }
 
+// Retrieve buyer information from session
+if (!isset($_SESSION['buyers'])) {
+    $_SESSION['buyers'] = array(); // Initialize an empty array for "buyers"
+}
+
+$buyers = $_SESSION['buyers'];
+
 // Connect to the database
 $db_host = "localhost";
 $db_user = "root";
@@ -18,6 +25,30 @@ $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+$buyerID = $_SESSION['buyerID'];
+$query = "SELECT buyerName, buyerEmail, buyerPhoneNumber, buyerAddress FROM buyers WHERE buyerID = '$buyerID'";
+$result = mysqli_query($conn, $query);
+
+
+$buyerInfo = mysqli_fetch_assoc($result);
+$buyerName = $buyerInfo['buyerName'];
+$buyerEmail = $buyerInfo['buyerEmail'];
+$buyerPhoneNumber = $buyerInfo['buyerPhoneNumber'];
+$buyerAddress = $buyerInfo['buyerAddress'];
+
+// Update buyer details if session variable is set
+if (isset($_SESSION['updatedBuyerInfo'])) {
+    $updatedBuyerInfo = $_SESSION['updatedBuyerInfo'];
+    $buyerName = $updatedBuyerInfo['buyerName'];
+    $buyerEmail = $updatedBuyerInfo['buyerEmail'];
+    $buyerPhoneNumber = $updatedBuyerInfo['buyerPhoneNumber'];
+    $buyerAddress = $updatedBuyerInfo['buyerAddress'];
+
+    // Clear the session variable
+    unset($_SESSION['updatedBuyerInfo']);
+}
+
 
 // Update product quantity in the cart
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_quantity'])) {
@@ -32,14 +63,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_quantity'])) {
         // Update quantity in cart
         $_SESSION['cart'][$productID] = $quantity;
     }
+
+    if (empty($_SESSION['cart'])) {
+        // Delete buyer information if cart is empty
+        unset($_SESSION['buyers']);
+
+        // Redirect to home1.php
+        header("Location: home1.php");
+        exit();
+    }
 }
 
 // Remove product from cart
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['remove_from_cart'])) {
     $productID = mysqli_real_escape_string($conn, $_POST['productID']);
-    
+
     // Remove product from cart
     unset($_SESSION['cart'][$productID]);
+
+    if (empty($_SESSION['cart'])) {
+        // Delete buyer information if cart is empty
+        unset($_SESSION['buyers']);
+
+    }
+
+    
 }
 
 // Retrieve products from the cart
@@ -76,137 +124,174 @@ if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
         );
 
         $totalCost += $subtotal;
+        $totalCost += 0; // Delivery charge is free, so add 0
+
     }
 }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $updatedBuyerInfo = array(
+        'buyerName' => $_POST['buyerName'],
+        'buyerEmail' => $_POST['buyerEmail'],
+        'buyerPhoneNumber' => $_POST['buyerPhoneNumber'],
+        'buyerAddress' => $_POST['buyerAddress']
+    );
+
+    // Store the updated buyer information in the session
+    $_SESSION['updatedBuyerInfo'] = $updatedBuyerInfo;
+
+    // Redirect to the cart page to see the updated information
+    header("Location: cart1.php");
+    exit();
+}
+
+// Update user details
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $buyerName = mysqli_real_escape_string($conn, $_POST['buyerName']);
+    $buyerPhoneNumber = mysqli_real_escape_string($conn, $_POST['buyerPhoneNumber']);
+    $buyerEmail = mysqli_real_escape_string($conn, $_POST['buyerEmail']);
+    $buyerAddress = mysqli_real_escape_string($conn, $_POST['buyerAddress']);
+
+    // Update user details in the database
+    $updateQuery = "UPDATE buyers SET buyerName = '$buyerName', buyerPhoneNumber = '$buyerPhoneNumber', buyerEmail = '$buyerEmail', buyerAddress = '$buyerAddress' WHERE buyerID = $buyerID";
+    $updateResult = mysqli_query($conn, $updateQuery);
+
+    if ($updateResult) {
+        $_SESSION['buyerName'] = $buyerName;
+        $_SESSION['buyerPhoneNumber'] = $buyerPhoneNumber;
+        $_SESSION['buyerEmail'] = $buyerEmail;
+        $_SESSION['buyerAddress'] = $buyerAddress;
+
+        $_SESSION['success_message'] = "Profile updated successfully.";
+        header("Location: home1.php");
+        exit();
+    } else {
+        $error_message = "Profile update failed. Please try again.";
+    }
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="style3.css">
     <title>Cart</title>
-    <link rel="stylesheet" type="text/css" href="style5.css">
-     <!-- Font Awesome -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+    <style>
+  
 
-<!-- Bootstrap CDN -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+.summary-box {
+    background-color: #f5f5f5;
+    border: 1px solid #ddd;
+    padding: 10px;
+    margin-bottom: 10px;
+    width: 100%; /* Adjust the width as needed */
+}
+
+.buyer-details {
+    width: 100%; /* Adjust the width as needed */
+    text-align:right;
+}
+
+.buyer-info {
+    margin-bottom: 20px;
+   
+}
+ 
+    </style>
 </head>
 <body>
+    <div class="topnav">
+        <img src="logo2.png" alt="Logo" width="150px">
+        <a href="logout1.php"><i class="fas fa-sign-out-alt"></i> LOGOUT</a>
+        <a href="userprofile1.php"><i class="fas fa-user"></i> USER PROFILE</a>
+        <a href="cart1.php" class="active"><i class="fas fa-shopping-cart"></i> CART</a>
+        <a href="home1.php"><i class="fas fa-home"></i> HOME</a>
+    </div>
+
+    <div class="shopping-cart">
+        <h2>My Cart</h2>
+        <hr>
+        <table>
+            <tr>
+                <th>Product</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Action</th>
+            </tr>
+            <?php foreach ($cartItems as $item) : ?>
+                <tr>
+                    <td><img src="<?php echo $item['productImage']; ?>" alt="Product Image" width="50px"></td>
+                    <td><?php echo $item['productName']; ?></td>
+                    <td>$<?php echo $item['productPrice']; ?></td>
+                    <td>
+                        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                            <input type="hidden" name="productID" value="<?php echo $item['productID']; ?>">
+                            <input type="number" name="quantity" value="<?php echo $item['quantity']; ?>" min="1">
+                            <button type="submit" name="update_quantity">Update</button>
+                        </form>
+                    </td>
+                    <td>$<?php echo $item['subtotal']; ?></td>
+                    <td>
+                        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                            <input type="hidden" name="productID" value="<?php echo $item['productID']; ?>">
+                            <button type="submit" name="remove_from_cart">Remove</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
 
 
-
-<nav>
-    <ul>
-    <li class="logo"><a href="home1.php"><img src="logo.png" alt="Logo" width="125px"></a></li>
-      <li><a href="home1.php">Home</a></li>
-     <li><a href="userprofile1.php">User Profile</a></li> 
-       <li><a href="cart1.php"> Cart</a></li>
-        <li style="float:right"><a href="logout1.php">Logout</a></li>
-    </ul>
-</nav>
-
-<div class="container-fluid">
-    <div class="row px-5">
-        <div class="col-md-7">
-            <div class="shopping-cart">
-                <h6>My Cart</h6>
-                <hr>
-
-    <?php if (!empty($cartItems)): ?>
-<table>
-<thead>
-<tr>
-<th>Product Image</th>
-<th>Product Name</th>
-<th>Price</th>
-<th>Product Description</th>
-<th>Quantity</th>
-<th>Subtotal</th>
-<th>Action</th>
-</tr>
-</thead>
-<tbody>
-<?php foreach ($cartItems as $item): ?>
-<tr>
-<td><img src="<?php echo $item['productImage']; ?>" alt="Product Image" style="width: 100px; height: 100px;"></td>
-<td><?php echo $item['productName']; ?></td>
-<td>$<?php echo $item['productPrice']; ?></td>
-<td>$<?php echo $item['productDescription']; ?></td>
-
-<td>
-<form method="post" action="cart1.php">
-<input type="hidden" name="productID" value="<?php echo $item['productID']; ?>">
-<input type="number" name="quantity" value="<?php echo $item['quantity']; ?>" min="1">
-<button type="submit" name="update_quantity">Update</button>
-</form>
-</td>
-<td>$<?php echo $item['subtotal']; ?></td>
-<td>
-<form method="post" action="cart1.php">
-<input type="hidden" name="productID" value="<?php echo $item['productID']; ?>">
-<button type="submit" name="remove_from_cart">Remove</button>
-</form>
-</td>
-</tr>
-<?php endforeach; ?>
-<tr>
-<td colspan="3" style="text-align: right;"><strong>Total:</strong></td>
-<td colspan="2">$<?php echo $totalCost; ?></td>
-</tr>
-</tbody>
-</table>
-<form method="post" action="checkout1.php">
-        <button type="submit" name="checkout">Checkout</button>
-    </form>
-<?php else: ?>
-    <p>Your cart is empty.</p>
-<?php endif; ?>
-
-</div>
-        </div>
-        <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
-
-            <div class="pt-4">
-                <h6>PRICE DETAILS</h6>
-                <hr>
-                <div class="row price-details">
-                    <div class="col-md-6">
-                        <?php
-                            if (isset($_SESSION['cart'])){
-                                $count  = count($_SESSION['cart']);
-                                echo "<h6>Price ($count items)</h6>";
-                            }else{
-                                echo "<h6>Price (0 items)</h6>";
-                            }
-                        ?>
-                        <h6>Delivery Charges</h6>
-                        <hr>
-                        <h6>Amount Payable</h6>
-                    </div>
-                    <div class="col-md-6">
-                        <h6>$<?php echo $totalCost; ?></h6>
-                        <h6 class="text-success">FREE</h6>
-                        <hr>
-                        <h6>$<?php
-                            echo $totalCost;
-                            ?></h6>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+    <div class="order-summary">
+    <h2>Order Summary</h2>
+    <div class="summary-box">
+        <table>
+            <tr>
+                <th>Product ID</th>
+                <th>Product</th>
+                <th>Price</th>
+            </tr>
+            <?php foreach ($cartItems as $item): ?>
+                <tr>
+                    <td><?php echo $item['productID']; ?></td>
+                    <td><strong><?php echo $item['productName']; ?></strong></td>
+                    <td>$<?php echo $item['productPrice']; ?></td>
+                </tr>
+                <tr>
+                    <td colspan="3" class="total">Subtotal: $<?php echo $item['subtotal']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+            <tr>
+                <td colspan="3" class="total">Delivery Charge: Free</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="total">Total Cost: $<?php echo $totalCost; ?></td>
+            </tr>
+        </table>
     </div>
 </div>
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-</div>
+
+
+
+<form method="post" action="checkout1.php">
+        <button type="submit" class="checkout-button" <?php echo empty($cartItems) ? 'disabled' : ''; ?>>Proceed to Checkout</button>
+    </form>
+         
+
+
+    
 </body>
 </html>
-<?php
-mysqli_close($conn);
-?>
